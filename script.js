9ae3fbdc979c27d131eb8eeaa690e3add6fe45cf
therock3t <=~Ak{]$'-p\228SpVs322VNn(;4A_RN>
2023-02-27 14:26:00 +0300

    fix bugs

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
-
 const blockInfo = document.querySelector('.block_info')
 const getBlock = document.querySelector('.block_get')
+const blockContent = document.querySelector('.block-get__content');
 
 function getUsers() {
     
@@ -10,29 +9,29 @@ function getUsers() {
         return response.json()
     })
     .then(data => {
-        data.forEach(i => {
-            let newBlockInfo = blockInfo.cloneNode(true)
-            getBlock.append(newBlockInfo)
+        console.log(data)
+        data.forEach(item => {
+            blockContent.innerHTML += (`
+                <div class="block_info" id="${item.id}">
+                <div class="block_get__title">
+                    <p class="title">${item.title}</p>
+                    <p class="description">${item.description}</p>
+                <div class="ingridients">
+                    <p></p>
+                    <ul class="ingredients_block">
 
-            let titles = newBlockInfo.querySelector('.title')
-            titles.innerText = i.title
-
-            let description = newBlockInfo.querySelector('.description')
-            description.innerText = i.description
-
-            let imgBlock = document.createElement("img");
-            let imgMain = newBlockInfo.querySelector('.block_get__img')
-            imgMain.append(imgBlock)
-            imgBlock.setAttribute('src', i.image)
-
-            let ingredients_block = newBlockInfo.querySelector('.ingredients_block')
-            console.log(ingredients_block)
-
-            i.ingredients.forEach(item => {
-                let li = document.createElement("li");
-                li.innerText = item
-                ingredients_block.append(li)
-            });
+                    </ul>
+                </div>
+            </div>
+            <div class="block_get__img">
+                <img src="${item.image}">
+            </div>
+        </div>
+            `)
+            if (typeof item.ingredients !== 'undefined') {
+                let list = document.querySelector(`.block_info[id="${item.id}"]`).querySelector('.ingredients_block');
+                item.ingredients.forEach(itemList => list.innerHTML +=(`<li>${itemList}</li>`));
+            }
         });
     })
     
